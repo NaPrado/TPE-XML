b5034302dd9d70d5a184a77bbd9a644e0c40507b
Nahuel Prado <141145234+NaPrado@users.noreply.github.com>
2024-06-05 10:30:16 -0300

    Co-authored-by: lpercich <lpercich@users.noreply.github.com> Co-authored-by: Mateo Buela <MateoBD@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,25 @@
+# TPE - Diseño y Procesamiento de Documentos XML
+
+El presente trabajo consiste en un programa que realiza una consulta a la página de SportRadar (https://developer.sportradar.com/) para obtener las estadísticas la competencia Nascar de algun año en particular.
+
+
+## Instrucciones:
+
+1. En primer lugar, se debe poseer una API_key válida para la página de SportRadar.
+
+2. Establecemos la variable de entorno API_KEY, usando el siguiente comando:
+    export API_KEY [Esto se debe remplazar por la key]
+
+3. Para ejecutar el programa, se debe correr la siguiente línea de códgio en la terminal: 
+
+   ```sh
+   ./tpe.sh "sc" 2015
+   ```
+Reemplazando "sc" por la competencia que se quiera consultar (sc, xf, cw, go, mc) y 2015 por el año solicitado (años validos desde 2013 hasta 2023).
+
+## Integrantes:
+
+    Nahuel Ignacio Prado    - 64276 - naprado@itba.edu.ar
+    Mateo Buela             - 64680 - mbuela@itba.edu.ar
+    Luana Percich           - legaj - lpercich@itba.edu.ar
+    Lorenzo Chiossone       - legaj - lchiossone@itba.edu.ar
--- tpe.sh
+++ tpe.sh
@@ -1,8 +1,8 @@
-SEASONS_FILE=seasons.xml
-SEASON_INFO_FILE=season_info.xml
-SEASON_LINEUPS_FILE=season_lineups.xml
-SEASON_DATA_FILE=season_data.xml
-MARKDOWN_FILE=season_page.md
+TYPES_FILE=types.xml
+TYPE_INFO_FILE=type_info.xml
+TYPE_LINEUPS_FILE=type_lineups.xml
+TYPE_DATA_FILE=type_data.xml
+MARKDOWN_FILE=type_page.md
 
 error=0
 invalid_arguments_number=0
@@ -38,10 +38,10 @@ fi
 
 if [ $error -eq 1 ]
 then
-    java net.sf.saxon.Query "season_id=$season_id" "invalid_arguments_number=$invalid_arguments_number" "null_api_key=$null_api_key" "information_not_found=$information_not_found" "year_error=$year_error" ./queries/extract_season_data.xq -o:./data/season_data.xml
-    echo Data generated at data/$SEASON_DATA_FILE
-    java net.sf.saxon.Transform -s:data/$SEASON_DATA_FILE -xsl:helpers/add_validation_schema.xsl -o:data/$SEASON_DATA_FILE
-    java net.sf.saxon.Transform -s:data/$SEASON_DATA_FILE -xsl:helpers/generate_markdown.xsl -o:data/$MARKDOWN_FILE
+    java net.sf.saxon.Query "type_id=$type_id" "invalid_arguments_number=$invalid_arguments_number" "null_api_key=$null_api_key" "information_not_found=$information_not_found" "year_error=$year_error" ./queries/extract_type_data.xq -o:./data/type_data.xml
+    echo Data generated at data/$TYPE_DATA_FILE
+    java net.sf.saxon.Transform -s:data/$TYPE_DATA_FILE -xsl:helpers/add_validation_schema.xsl -o:data/$TYPE_DATA_FILE
+    java net.sf.saxon.Transform -s:data/$TYPE_DATA_FILE -xsl:helpers/generate_markdown.xsl -o:data/$MARKDOWN_FILE
     echo Page generated at data/$MARKDOWN_FILE
     exit 1
 fi
@@ -49,26 +49,26 @@ fi
 name_prefix=$1
 year=$2
 
-# Call to seasons endpoint
-./scripts/seasons.sh $SEASONS_FILE
+# Call to types endpoint
+./scripts/types.sh $TYPES_FILE
 
-# Get season id for name_prefix and year received
-season_id=$(java net.sf.saxon.Query "seasons_file=$SEASONS_FILE" "season_prefix=$name_prefix" "season_year=$year" ./queries/extract_season_id.xq)
+# Get type id for name_prefix and year received
+type_id=$(java net.sf.saxon.Query "types_file=$TYPES_FILE" "type_prefix=$name_prefix" "type_year=$year" ./queries/extract_type_id.xq)
 
-if [ -z "$season_id" ]
+if [ -z "$type_id" ]
 then
-    season_id="nullid"
+    type_id="nullid"
     information_not_found=1
     error=1
 fi
 
 # Call to information and lineups endpoint
-./scripts/season_info.sh $SEASON_INFO_FILE $season_id
-./scripts/season_lineups.sh $SEASON_LINEUPS_FILE $season_id
+./scripts/type_info.sh $TYPE_INFO_FILE $type_id
+./scripts/type_lineups.sh $TYPE_LINEUPS_FILE $type_id
 
-java net.sf.saxon.Query "season_id=$season_id" "invalid_arguments_number=$invalid_arguments_number" "null_api_key=$null_api_key" "information_not_found=$information_not_found" "year_error=$year_error" ./queries/extract_season_data.xq -o:./data/season_data.xml
+java net.sf.saxon.Query "type_id=$type_id" "invalid_arguments_number=$invalid_arguments_number" "null_api_key=$null_api_key" "information_not_found=$information_not_found" "year_error=$year_error" ./queries/extract_type_data.xq -o:./data/type_data.xml
 
-java net.sf.saxon.Transform -s:data/$SEASON_DATA_FILE -xsl:helpers/add_validation_schema.xsl -o:data/$SEASON_DATA_FILE
-echo Data generated at data/$SEASON_DATA_FILE
-java net.sf.saxon.Transform -s:data/$SEASON_DATA_FILE -xsl:helpers/generate_markdown.xsl -o:data/$MARKDOWN_FILE
+java net.sf.saxon.Transform -s:data/$TYPE_DATA_FILE -xsl:helpers/add_validation_schema.xsl -o:data/$TYPE_DATA_FILE
+echo Data generated at data/$TYPE_DATA_FILE
+java net.sf.saxon.Transform -s:data/$TYPE_DATA_FILE -xsl:helpers/generate_markdown.xsl -o:data/$MARKDOWN_FILE
 echo Page generated at data/$MARKDOWN_FILE
